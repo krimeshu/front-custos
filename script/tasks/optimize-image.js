092d506c2a1f19713feaf4133b6995a81b9478ef
krimeshu <krimeshu@gmail.com>
2017-06-08 17:47:13 +0800

    修复部分PNG压缩异常的问题

--- script/tasks/optimize-image.js
+++ script/tasks/optimize-image.js
@@ -3,12 +3,15 @@
  */
 
 var _path = require('path'),
+    _os = require('os'),
 
     PluginLoader = require('../plugin-loader.js'),
     plugins = PluginLoader.plugins,
 
     Utils = require('../utils.js'),
     Timer = require('../timer.js');
+
+var cacheDirName = 'imagemin-cache';
 
 // 优化图片：
 // - Png图片有损压缩（PngQuant）
@@ -26,15 +29,19 @@ module.exports = {
 
             gulp.src(_path.resolve(workDir, '**/*.{jpg,gif,png,svg}'))
                 .pipe(plugins.plumber({ 'errorHandler': errorHandler }))
-                .pipe(plugins.cache(plugins.imagemin([
-                    plugins.imagemin.gifsicle({ interlaced: true }),
-                    plugins.imagemin.jpegtran({ progressive: true }),
-                    // plugins.imagemin.optipng({optimizationLevel: 5}),
-                    plugins.pngquant({ quality: '50-80', speed: 4 }),
-                    plugins.imagemin.svgo({ plugins: [{ removeViewBox: false }] })
-                ]), {
-                        fileCache: new plugins.cache.Cache({ cacheDirName: 'imagemin-cache' })
-                    }))
+                .pipe(plugins.cache(
+                    plugins.imagemin([
+                        plugins.imagemin.gifsicle({ interlaced: true }),
+                        plugins.imagemin.jpegtran({ progressive: true }),
+                        // plugins.imagemin.optipng({optimizationLevel: 5}),
+                        plugins.pngquant({ quality: '50-80', speed: 4 }),
+                        plugins.imagemin.svgo({ plugins: [{ removeViewBox: false }] })
+                    ]), {
+                        fileCache: new plugins.cache.Cache({ cacheDirName: cacheDirName })
+                    }
+                ).on('error', function () {
+                    this.emit('end');
+                }))
                 .pipe(gulp.dest(workDir))
                 .on('end', function () {
                     logId && console.useId && console.useId(logId);
@@ -43,17 +50,28 @@ module.exports = {
                 });
         };
     },
-    'clear_cache': function (console, taskName) {
+    'clear_cache': function (console, errorHandler, taskName) {
         return function (done) {
             var timer = new Timer();
             var logId = console.genUniqueId && console.genUniqueId();
             logId && console.useId && console.useId(logId);
             console.log(Utils.formatTime('[HH:mm:ss.fff]'), taskName + ' 清空缓存中……');
-            return plugins.cache.clearAll(function () {
+
+            var cacheDirPath = _path.resolve(_os.tmpdir(), cacheDirName);
+
+            function afterClean() {
                 logId && console.useId && console.useId(logId);
                 console.log(Utils.formatTime('[HH:mm:ss.fff]'), taskName + ' 缓存清空完毕。（' + timer.getTime() + 'ms）');
                 done();
-            });
+            }
+            var cleanFailed = function (e) {
+                var err = new Error('图片缓存目录清理失败，请检查目录是否被占用');
+                err.detail = e;
+                errorHandler(err);
+
+                afterClean();
+            };
+            plugins.del(cacheDirPath, { force: true }).then(afterClean).catch(cleanFailed);
         };
     }
 };
