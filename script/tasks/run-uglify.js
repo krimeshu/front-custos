6000781ab3b69b0552afac37104b07e4fb2436d4
krimeshu <krimeshu@gmail.com>
2017-08-17 11:28:50 +0800

    run-uglify 默认只打包入口脚本

--- script/tasks/run-uglify.js
+++ script/tasks/run-uglify.js
@@ -23,8 +23,22 @@ module.exports = function (console, gulp, params, errorHandler, taskName) {
             sourceMappingURL = smOpt.mappingUrl;
 
         var workDir = params.workDir,
-            entry = params.bundleEntry;
+            jsOpt = params.jsOpt || {},
+            entry = jsOpt.bundleEntry;
 
+
+        if (typeof entry == 'string' && entry.length) {
+            entry = entry.split(/\r?\n/g).map((entryPath) => _path.resolve(workDir, entryPath));
+        }
+        if (Array.isArray(entry)) {
+            if (entry.some((e) => typeof e != 'string')) {
+                console.error('打包入口 entry 类型错误（应为 string/array of string）');
+            }
+        }
+        if (params.bundleEntry) {
+            // 其它任务中找到的打包入口脚本
+            entry = (entry || []).concat(params.bundleEntry);
+        }
         if (!entry || !entry.length) {
             entry = _path.resolve(workDir, '**/*@(.js)');
         }
