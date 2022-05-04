978bd8e640ed3c0202b31c6d903f2f2e58ec0cb4
krimeshu <krimeshu@tencent.com>
2022-05-04 21:26:09 +0800

    feat: 移除 pngquant

--- script/tasks/optimize-image.js
+++ script/tasks/optimize-image.js
@@ -13,12 +13,10 @@ var _path = require('path'),
 
 PluginLoader.add({ 'del': () => require('del') });
 PluginLoader.add({ 'imagemin': () => require('gulp-imagemin') });
-PluginLoader.add({ 'pngquant': () => require('imagemin-pngquant') });
 
 var cacheDirName = 'imagemin-cache';
 
 // 优化图片：
-// - Png图片有损压缩（PngQuant）
 // - Jpg图片转为渐进式
 // - Gif图片转为隔行加载
 module.exports = {
@@ -37,8 +35,7 @@ module.exports = {
                     plugins.imagemin([
                         plugins.imagemin.gifsicle({ interlaced: true }),
                         plugins.imagemin.jpegtran({ progressive: true }),
-                        // plugins.imagemin.optipng({ optimizationLevel: 5 }),
-                        plugins.pngquant({ quality: '50-80', speed: 4 }),
+                        plugins.imagemin.optipng({ optimizationLevel: 5 }),
                         plugins.imagemin.svgo({ plugins: [{ removeViewBox: false }] })
                     ]), {
                     fileCache: new plugins.cache.Cache({ cacheDirName: cacheDirName })
